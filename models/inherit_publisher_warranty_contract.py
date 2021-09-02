9f688b6347f6e212f78bc03342a04e33000e1f98
Lê Anh Tuấn <justin.le.1105@gmail.com>
2021-09-03 00:03:17 +0700

    migrate to 14.0

--- models/inherit_publisher_warranty_contract.py
+++ models/inherit_publisher_warranty_contract.py
@@ -30,19 +30,15 @@ class InheritedPublisherWarrantyContract(AbstractModel):
                     _("Error during communication with the publisher warranty server.")
                 )
             # old behavior based on res.log; now on mail.message, that is not necessarily installed
-            user = self.env["res.users"].sudo().browse(SUPERUSER_ID)
-            poster = self.sudo().env.ref("mail.channel_all_employees")
+            user = self.env['res.users'].sudo().browse(SUPERUSER_ID)
+            poster = self.sudo().env.ref('mail.channel_all_employees')
             if not (poster and poster.exists()):
                 if not user.exists():
                     return True
                 poster = user
             for message in result["messages"]:
                 try:
-                    poster.message_post(
-                        body=message,
-                        subtype="mt_comment",
-                        partner_ids=[user.partner_id.id],
-                    )
+                    poster.message_post(body=message, subtype_xmlid='mail.mt_comment', partner_ids=[user.partner_id.id])
                 except Exception:
                     pass
 
@@ -52,37 +48,15 @@ class InheritedPublisherWarrantyContract(AbstractModel):
             except KeyError:
                 pass
 
-            if result.get("enterprise_info"):
+            if result.get('enterprise_info'):
                 # Update expiration date
-                set_param = self.env["ir.config_parameter"].sudo().set_param
-                set_param(
-                    "database.expiration_date",
-                    result["enterprise_info"].get("expiration_date"),
-                )
-                set_param(
-                    "database.expiration_reason",
-                    result["enterprise_info"].get("expiration_reason", "trial"),
-                )
-                set_param(
-                    "database.enterprise_code",
-                    result["enterprise_info"].get("enterprise_code"),
-                )
-                set_param(
-                    "database.already_linked_subscription_url",
-                    result["enterprise_info"].get(
-                        "database_already_linked_subscription_url"
-                    ),
-                )
-                set_param(
-                    "database.already_linked_email",
-                    result["enterprise_info"].get("database_already_linked_email"),
-                )
-                set_param(
-                    "database.already_linked_send_mail_url",
-                    result["enterprise_info"].get(
-                        "database_already_linked_send_mail_url"
-                    ),
-                )
+                set_param = self.env['ir.config_parameter'].sudo().set_param
+                set_param('database.expiration_date', result['enterprise_info'].get('expiration_date'))
+                set_param('database.expiration_reason', result['enterprise_info'].get('expiration_reason', 'trial'))
+                set_param('database.enterprise_code', result['enterprise_info'].get('enterprise_code'))
+                set_param('database.already_linked_subscription_url', result['enterprise_info'].get('database_already_linked_subscription_url'))
+                set_param('database.already_linked_email', result['enterprise_info'].get('database_already_linked_email'))
+                set_param('database.already_linked_send_mail_url', result['enterprise_info'].get('database_already_linked_send_mail_url'))
 
         except Exception:
             if cron_mode:
